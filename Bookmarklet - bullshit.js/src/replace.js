216ea6b510b469711003598dac545d383debfa0a
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-11-20 18:50:54 -0500

    Create replace.js

---
+++ Bookmarklet - bullshit.js/src/replace.js
@@ -0,0 +1,33 @@
+import findAndReplaceDOMText from 'findandreplacedomtext';
+import bullshitTerms from './terms';
+
+function revealBullshit({text}) {
+    const c = text.charAt(0);
+    const last = text.length - 1;
+    let bullshit = `${c === c.toUpperCase() ? 'B' : 'b'}ullshit`;
+
+    if (text.substr(last - 2) === 'ing') {
+        bullshit += 'ting';
+    } else if (text.charAt(last - 1) !== 's' && text.charAt(last) === 's') {
+        bullshit += 's';
+    } else if (text.charAt(last - 2) !== 'e' && text.substr(last - 1) === 'ed') {
+        bullshit += 'ted';
+    } else if (text.charAt(last - 2) !== ('o' || 'e') && text.substr(last - 1) === ('or' || 'er')) {
+        bullshit += 'ter';
+    }
+
+    const abbr = document.createElement('abbr');
+    abbr.style.color = 'red';
+    abbr.title = text;
+    abbr.innerHTML = bullshit;
+
+    return abbr;
+}
+
+const bullshitRe = new RegExp(`\\b(${bullshitTerms.join('|')})\\b`, 'gi');
+
+findAndReplaceDOMText(document.body, {
+    find: bullshitRe,
+    replace: revealBullshit,
+    preset: 'prose'
+});
