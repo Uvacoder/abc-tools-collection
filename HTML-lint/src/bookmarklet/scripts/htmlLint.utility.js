40e76493eb16c4a6ea9ff4eac5ee98e11f2f0cb3
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-11-20 13:14:13 -0500

    Add files via upload

---
+++ HTML-lint/src/bookmarklet/scripts/htmlLint.utility.js
@@ -0,0 +1,23 @@
+(function (htmlLint) {
+	'use strict';
+
+	htmlLint.utility = {
+		css: 'https://curtisj44.github.io/HTML-Lint/dist/html-lint.min.css',
+
+		error: function (message) {
+			return '<span class="html-lint-error">' + (message || 'missing tag') + '</span>';
+		},
+
+		jQueryAdded: false,
+
+		jQuery: ['1.12.4', '3.2.1'],
+		jQueryUI: '1.12.0',
+		Modernizr: '3.3.1',
+		MooTools: '1.6.0',
+		RequireJS: '2.2.0'
+	};
+
+	// TODO - organize this better
+	htmlLint.preInit();
+
+}(window.htmlLint = window.htmlLint || {}));
